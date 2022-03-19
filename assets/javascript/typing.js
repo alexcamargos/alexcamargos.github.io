0de2d2f1a8f9d21f2217abbf9d0bdf0826a71277
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-03-19 14:41:28 -0300

    add HERO SECTION

--- assets/javascript/typing.js
+++ assets/javascript/typing.js
@@ -14,3 +14,40 @@
 #
 # -------------------------------------------------------------------------------------------------
 */
+
+// TypeIt v8.3.3 - The most versatile animated typing utility on the planet.
+
+new TypeIt("#typing-effect", {
+    waitUntilVisible: true,
+    speed: 150,
+    deleteSpeed: 75,
+    lifeLike: true,
+    breakLines: false,
+    loop: true,
+})
+    .type("student.", {
+        delay: 300,
+    })
+    .pause(1000)
+    .delete(8)
+    .type("developer.", {
+        delay: 300,
+    })
+    .pause(1000)
+    .delete(10)
+    .type("back-end developer.", {
+        delay: 300,
+    })
+    .pause(1000)
+    .delete(19)
+    .type("front-end developer.", {
+        delay: 300,
+    })
+    .pause(1000)
+    .delete(20)
+    .type("Junior full stack develope.", {
+        delay: 300,
+    })
+    .pause(5000)
+    .delete(27)
+    .go();
