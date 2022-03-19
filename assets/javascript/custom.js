5f9c004000d1829c39ba242dc712624f28bc5562
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-03-19 14:06:10 -0300

    add some jquery custom functions

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -14,3 +14,125 @@
 #
 # -------------------------------------------------------------------------------------------------
 */
+
+(function ($, window, document, undefined) {
+    "use strict";
+    var $winW = function () {
+        return $(window).width();
+    };
+    var $winH = function () {
+        return $(window).height();
+    };
+    var $screensize = function (element) {
+        $(element).width($winW()).height($winH());
+    };
+    var screencheck = function (mediasize) {
+        if (typeof window.matchMedia !== "undefined") {
+            var screensize = window.matchMedia(
+                "(max-width:" + mediasize + "px)"
+            );
+            if (screensize.matches) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if ($winW() <= mediasize) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+    $(document).ready(function () {
+        $(window).on("load", function () {
+            $(".preloader").fadeOut();
+            $(".animated-row").each(function () {
+                var $this = $(this);
+                $this.find(".animate").each(function (i) {
+                    var $item = $(this);
+                    var animation = $item.data("animate");
+                    $item.on("inview", function (event, isInView) {
+                        if (isInView) {
+                            setTimeout(function () {
+                                $item
+                                    .addClass("animated " + animation)
+                                    .removeClass("animate");
+                            }, i * 50);
+                        } else if (!screencheck(767)) {
+                            $item
+                                .removeClass("animated " + animation)
+                                .addClass("animate");
+                        }
+                    });
+                });
+            });
+        });
+        
+        if ($(".fullpage-default").length) {
+            var myFullpage = new fullpage(".fullpage-default", {
+                licenseKey: " C7F41B00-5E824594-9A5EFB99-B556A3D5",
+                anchors: [
+                    "slide01",
+                    "slide02",
+                    "slide03",
+                    "slide04",
+                    "slide05",
+                    "slide06",
+                ],
+                menu: "#nav",
+                lazyLoad: true,
+                navigation: true,
+                navigationPosition: "right",
+                scrollOverflow: true,
+                responsiveWidth: 768,
+                responsiveHeight: 600,
+                responsiveSlides: true,
+            });
+        }
+        $(document)
+            .on("click", ".navbar-toggle", function () {
+                $(".navbar-collapse").slideToggle(300);
+                return false;
+            })
+            .on("click", ".navigation-menu > li > a", function () {
+                $(".navbar-collapse").slideUp(300);
+            })
+            .on("click", ".next-section", function () {
+                fullpage_api.moveSectionDown();
+            });
+        $(".facts-row").on("inview", function (event, isInView) {
+            $(".count-number").each(function () {
+                $(this)
+                    .prop("Counter", 0)
+                    .animate(
+                        {
+                            Counter: $(this).text(),
+                        },
+                        {
+                            duration: 1000,
+                            easing: "swing",
+                            step: function (now) {
+                                $(this).text(Math.ceil(now));
+                            },
+                        }
+                    );
+                setTimeout(function () {
+                    $(".count-number")
+                        .removeClass("count-number")
+                        .addClass("counted");
+                }, 1000);
+            });
+        });
+        $(".skills-row").on("inview", function (event, isInView) {
+            $(this).addClass("view");
+        });
+        $(document)
+            .on("click", ".menu-trigger", function () {
+                $("body").toggleClass("sidemenu-open");
+            })
+            .on("click", ".side-menu .navbar-nav li a", function () {
+                $("body").removeClass("sidemenu-open");
+            });
+    });
+})(jQuery, window, document);
