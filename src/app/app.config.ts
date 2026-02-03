9be0241a995e382cea4cdaf2a7b27687f792bb29
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-02-03 20:56:44 -0300

    chore(config): configuração inicial do workspace angular v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,35 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader, TRANSLATE_HTTP_LOADER_CONFIG } from '@ngx-translate/http-loader';
+
+// Factory function obrigatória para AOT compilation
+export function HttpLoaderFactory() {
+  return new TranslateHttpLoader();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(), // Essencial para carregar os JSONs
+    {
+      provide: TRANSLATE_HTTP_LOADER_CONFIG,
+      useValue: {
+        prefix: './assets/i18n/',
+        suffix: '.json'
+      }
+    },
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+        },
+        defaultLanguage: 'en'
+      })
+    )
   ]
 };
