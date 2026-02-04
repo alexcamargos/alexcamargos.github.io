ce70ba69d5f8310de75f2f724be408d09b6590eb
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-02-03 22:11:01 -0300

    feat(ui): aprimoramentos no componente Header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,5 @@
 import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ThemeService } from '../../core/services/theme';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
@@ -6,7 +7,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -14,9 +15,18 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   themeService = inject(ThemeService);
   translate = inject(TranslateService);
+  isMobileMenuOpen = false;
 
   toggleTheme() {
     this.themeService.updateDarkMode();
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
   }
 
   changeLanguage(lang: string) {
