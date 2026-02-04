aea268cddaae6fc6ba1b5d17369128d163faafbd
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-02-03 21:01:28 -0300

    feat(app): layout principal e definição de rotas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('portfolio-v1');
+  private translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.addLangs(['en', 'pt']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
